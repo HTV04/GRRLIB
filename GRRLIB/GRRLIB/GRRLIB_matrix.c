3d827b24e026a894bebd0866d9dfd8d0042f89bc
Harrison <53527582+HTV04@users.noreply.github.com>
2022-03-19 15:20:01 -0400

    Fix GRRLIB_SetMatrix

--- GRRLIB/GRRLIB/GRRLIB_matrix.c
+++ GRRLIB/GRRLIB/GRRLIB_matrix.c
@@ -22,9 +22,9 @@ THE SOFTWARE.
 
 #include <grrlib.h>
 
-extern  GRRLIB_drawSettings  GRRLIB_Settings;
-extern  Mtx                  GXmodelView2D;
-extern  guVector             axis2D;
+extern GRRLIB_drawSettings GRRLIB_Settings;
+extern Mtx GXmodelView2D;
+extern guVector axis2D;
 
 /**
  * Get the current matrix as a new matrix object.
@@ -32,7 +32,7 @@ extern  guVector             axis2D;
  * @see GRRLIB_matrix
  */
 GRRLIB_matrix GRRLIB_GetMatrix (void) {
-    GRRLIB_matrix  matrixObject;
+    GRRLIB_matrix matrixObject;
 
     guMtxCopy(GXmodelView2D, matrixObject.matrix);
 
@@ -45,6 +45,7 @@ GRRLIB_matrix GRRLIB_GetMatrix (void) {
  */
 void GRRLIB_SetMatrix (GRRLIB_matrix *matrixObject) {
     guMtxCopy(matrixObject->matrix, GXmodelView2D);
+    GX_LoadPosMtxImm(matrixObject->matrix, GX_PNMTX0);
 }
 
 /**
@@ -53,12 +54,12 @@ void GRRLIB_SetMatrix (GRRLIB_matrix *matrixObject) {
  * @param scaleY The scaling in the direction of the y-axis.
  */
 void GRRLIB_Scale (f32 scaleX, f32 scaleY) {
-    Mtx  m;
+    Mtx m;
 
-    guMtxIdentity  (m);
+    guMtxIdentity(m);
     guMtxScaleApply(m, m, scaleX, scaleY, 1.0);
 
-    guMtxConcat     (GXmodelView2D, m, GXmodelView2D);
+    guMtxConcat(GXmodelView2D, m, GXmodelView2D);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
 
@@ -67,11 +68,11 @@ void GRRLIB_Scale (f32 scaleX, f32 scaleY) {
  * @param degrees The amount to rotate the current matrix in degrees.
  */
 void GRRLIB_Rotate (f32 degrees) {
-    Mtx  m;
+    Mtx m;
 
     guMtxRotAxisDeg(m, &axis2D, degrees);
 
-    guMtxConcat     (GXmodelView2D, m, GXmodelView2D);
+    guMtxConcat(GXmodelView2D, m, GXmodelView2D);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
 
@@ -81,12 +82,12 @@ void GRRLIB_Rotate (f32 degrees) {
  * @param posY The translation relative to the y-axis.
  */
 void GRRLIB_Translate (f32 posX, f32 posY) {
-    Mtx  m;
+    Mtx m;
 
-    guMtxIdentity  (m);
+    guMtxIdentity(m);
     guMtxTransApply(m, m, posX, posY, 0.0);
 
-    guMtxConcat     (GXmodelView2D, m, GXmodelView2D);
+    guMtxConcat(GXmodelView2D, m, GXmodelView2D);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
 
@@ -101,13 +102,13 @@ void GRRLIB_Translate (f32 posX, f32 posY) {
 void GRRLIB_Transform (f32 scaleX, f32 scaleY, f32 degrees, f32 posX, f32 posY) {
     Mtx m1, m2, m;
 
-    guMtxIdentity  (m1);
+    guMtxIdentity(m1);
     guMtxScaleApply(m1, m1, scaleX, scaleY, 1.0);
     guMtxRotAxisDeg(m2, &axis2D, degrees);
-    guMtxConcat    (m1, m2, m);
+    guMtxConcat(m1, m2, m);
     guMtxTransApply(m, m, posX, posY, 0.0);
 
-    guMtxConcat     (GXmodelView2D, m, GXmodelView2D);
+    guMtxConcat(GXmodelView2D, m, GXmodelView2D);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
 
@@ -122,13 +123,13 @@ void GRRLIB_Transform (f32 scaleX, f32 scaleY, f32 degrees, f32 posX, f32 posY) 
 void GRRLIB_TransformInv (f32 scaleX, f32 scaleY, f32 posX, f32 posY, f32 degrees) {
     Mtx m1, m2, m;
 
-    guMtxIdentity  (m1);
+    guMtxIdentity(m1);
     guMtxScaleApply(m1, m1, scaleX, scaleY, 1.0);
     guMtxTransApply(m2, m2, posX, posY, 0.0);
-    guMtxConcat    (m1, m2, m);
+    guMtxConcat(m1, m2, m);
     guMtxRotAxisDeg(m, &axis2D, degrees);
 
-    guMtxConcat     (GXmodelView2D, m, GXmodelView2D);
+    guMtxConcat(GXmodelView2D, m, GXmodelView2D);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
 
@@ -136,7 +137,7 @@ void GRRLIB_TransformInv (f32 scaleX, f32 scaleY, f32 posX, f32 posY, f32 degree
  * Reset the current matrix.
  */
 void GRRLIB_Origin (void) {
-    guMtxIdentity   (GXmodelView2D);
-    guMtxTransApply (GXmodelView2D, GXmodelView2D, 0.0, 0.0, -100.0);
+    guMtxIdentity(GXmodelView2D);
+    guMtxTransApply(GXmodelView2D, GXmodelView2D, 0.0, 0.0, -100.0);
     GX_LoadPosMtxImm(GXmodelView2D, GX_PNMTX0);
 }
